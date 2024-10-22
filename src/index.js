30fd279e980625fdf1f91a3e6962255eee7c070f
Yoos <yoosweeb@gmail.com>
2024-10-22 11:53:43 +0300

    so I'll redo everything as a fresh start to learn new stuff......... and yeah that's about is

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import { parse } from 'postcss';
 import './main.css';
 import './style.css';
+// import * as something from "./declarations.js"
+//Here goes my declarations
+const input_todo = document.getElementById("todo_text")
+const plus_todo = document.getElementById("add_plus_btn") 
+const alert_todo = document.getElementById("Alert")
+const list_todo = document.getElementById("list_items")
+//Here they end
+
+let todo = JSON.parse(localStorage.getItem("todo_list"))
+if (!todo) {
+    todo = []
+}
 
 console.log("just testing if it is working my index.js")
 
--- src/render.js
+++ src/render.js
@@ -1,3 +1,3 @@
 function render() {
-    <li class="list-name"></li>
+    
 }

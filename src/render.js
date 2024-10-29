08923a3e50a2a59939cfa450d42c25bc6a21098f
Yoos <yoosweeb@gmail.com>
2024-10-29 12:59:10 +0300

    nothing to add just the comments

--- src/render.js
+++ src/render.js
@@ -3,10 +3,19 @@ const containerList = document.querySelector("[data-lists]")
 let lists = ["name", "todo"]
 export function render() {
     clearElement(containerList)
-    lists.forEach(list => {
-        const listElement = document.createElement("li")
-        listElement.classList.add("active_list")
-        listElement.innerText = list
-        containerList.appendChild(listElement)
-    });
+// Iterates over each item in the 'lists' array
+lists.forEach(list => {
+    // Creates a new 'li' element for each item
+    const listElement = document.createElement("li");
+
+    // Adds the 'active_list' class to the new 'li' element
+    listElement.classList.add("active_list");
+
+    // Sets the text of the 'li' element to the current item in the 'lists' array
+    listElement.innerText = list;
+
+    // Appends the new 'li' element to the 'containerList' element
+    containerList.appendChild(listElement);
+});
+
 }

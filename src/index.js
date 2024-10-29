d76914bed0671a48eb337235641bae3901862863
Yoos <yoosweeb@gmail.com>
2024-10-29 12:50:49 +0300

    AND WE BACK BABY, anyway So I might ask myself what did i do? answer is, fuck if i know! so first created a module for each function i knew I'll make, one for render and one for clearing shit up and understood that exporting the function from the very start is faster and good then import it to index Secondly i created array called lists to hold my shit of content. Thirdly i knew that i can  consider render as the main function so i called the function clear for it Fourth i commented what i have done here so check it when i need it, okay? and that's about it.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { parse } from 'postcss';
 import './main.css';
 import './style.css';
-// import * as something from "./declarations.js"
+// import * as something from "./declarations.js"|
 console.log("just testing if it is working my index.js")
+// My Imports are here
+import { clearElement } from "./clear.js";
+import { render } from "./render.js";
 
+// console.log(clearElement())
+// I'll start from here I'll make the declerations here
 
+const containerList = document.querySelector("[data-lists]")
+
+render()
--- src/render.js
+++ src/render.js
@@ -1,3 +1,12 @@
-function render() {
-    
+import { clearElement } from "./clear.js";
+const containerList = document.querySelector("[data-lists]")
+let lists = ["name", "todo"]
+export function render() {
+    clearElement(containerList)
+    lists.forEach(list => {
+        const listElement = document.createElement("li")
+        listElement.classList.add("active_list")
+        listElement.innerText = list
+        containerList.appendChild(listElement)
+    });
 }
